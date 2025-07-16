7b5687b85affa164483b5a8a11376ff314ffdb8a
sasquach45932 <sasquach@citromail.hu>
2025-07-16 17:08:09 +0200

    v1.1

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Built using the [ItemPiles System Template](https://github.com/fantasycalendar/F
 
 ## Usage
 Simply install this alongside Item Piles. Once installed and activated it will provide Item Piles with the configuration it needs to work properly in the SotDL system.
+
+# Installation
+
+1. On Foundry VTT's **Configuration and Setup** screen, go to **Add-on Modules**
+2. Click **Install Module**
+3. In the Manifest URL field, paste: `https://github.com/sasquach45932/item-piles-demonlord/releases/latest/download/module.json`
+4. Click **Install** next to the pasted Manifest URL
--- module.js
+++ module.js
@@ -1,38 +1,38 @@
-Hooks.once('item-piles-ready', async () => {
+Hooks.once("item-piles-ready", async () => {
   const data = {
     VERSION: "1.0.0",
 
     // The actor class type is the type of actor that will be used for the default item pile actor that is created on first item drop.
-    ACTOR_CLASS_TYPE: 'character',
+    ACTOR_CLASS_TYPE: "character",
 
     // The item class type is the type of item that will be used for the default loot item
-    ITEM_CLASS_LOOT_TYPE: 'item',
+    ITEM_CLASS_LOOT_TYPE: "item",
 
     // The item class type is the type of item that will be used for the default weapon item
-    ITEM_CLASS_WEAPON_TYPE: 'weapon',
+    ITEM_CLASS_WEAPON_TYPE: "weapon",
 
     // The item class type is the type of item that will be used for the default equipment item
-    ITEM_CLASS_EQUIPMENT_TYPE: 'item',
+    ITEM_CLASS_EQUIPMENT_TYPE: "item",
 
     // The item quantity attribute is the path to the attribute on items that denote how many of that item that exists
-    ITEM_QUANTITY_ATTRIBUTE: 'system.quantity',
+    ITEM_QUANTITY_ATTRIBUTE: "system.quantity",
 
     // The item price attribute is the path to the attribute on each item that determine how much it costs
-    ITEM_PRICE_ATTRIBUTE: 'system.value',
+    ITEM_PRICE_ATTRIBUTE: "system.value",
 
     // The quantity for price attribute is the path to the attribute on each item that determine how many you get for its price
-    QUANTITY_FOR_PRICE_ATTRIBUTE: 'system.quantity',
+    QUANTITY_FOR_PRICE_ATTRIBUTE: "system.quantity",
 
     // Item types and the filters actively remove items from the item pile inventory UI that users cannot loot, such as spells, feats, and classes
     ITEM_FILTERS: [
       {
-        path: 'type',
-        filters: 'ancestry,creaturerole,endoftheround,feature,language,path,profession,specialaction,spell,talent',
+        path: "type",
+        filters: "ancestry,creaturerole,endoftheround,feature,language,path,profession,specialaction,spell,talent",
       },
     ],
 
     // Item similarities determines how item piles detect similarities and differences in the system
-    ITEM_SIMILARITIES: ['name', 'type'],
+    ITEM_SIMILARITIES: ["name", "type"],
 
     // This function is an optional system handler that specifically transforms an item when it is added to actors, eg turns it into a spell scroll if it was a spell
     ITEM_TRANSFORMER: async itemData => {
@@ -43,13 +43,13 @@ Hooks.once('item-piles-ready', async () => {
       let itemCost = item.system.value
       itemCost = itemCost.trim()
 
-      let ssValue = itemCost === '' ? 0 : parseInt(itemCost.replace(/\D/g, ''))
+      let ssValue = itemCost === "" ? 0 : parseInt(itemCost.replace(/\D/g, ""))
 
       const overallCost = ssValue
-      let priceDenomination = itemCost.replace(/\d/g, '').trim() === '' ? 'ss' : itemCost.replace(/\d/g, '').trim()
+      let priceDenomination = itemCost.replace(/\d/g, "").trim() === "" ? "ss" : itemCost.replace(/\d/g, "").trim()
       if (priceDenomination) {
         const currencyDenomination = currencies
-          .filter(currency => currency.type === 'attribute')
+          .filter(currency => currency.type === "attribute")
           .find(currency => {
             return currency.data.path.toLowerCase().endsWith(priceDenomination)
           })
@@ -63,16 +63,16 @@ Hooks.once('item-piles-ready', async () => {
     PILE_DEFAULTS: {
       merchantColumns: [
         {
-          label: 'DL.Availability',
-          path: 'system.availability',
-          formatting: '{#}',
+          label: "DL.Availability",
+          path: "system.availability",
+          formatting: "{#}",
           buying: true,
           selling: true,
           mapping: {
-            C: 'DL.AvailabilityC',
-            U: 'DL.AvailabilityU',
-            R: 'DL.AvailabilityR',
-            E: 'DL.AvailabilityE',
+            C: "DL.AvailabilityC",
+            U: "DL.AvailabilityU",
+            R: "DL.AvailabilityR",
+            E: "DL.AvailabilityE",
           },
         },
       ],
@@ -83,45 +83,45 @@ Hooks.once('item-piles-ready', async () => {
     // In the case of items, it is recommended you export the item with `.toObject()`, put it into `data.item`, and strip out any module data
     CURRENCIES: [
       {
-        type: 'attribute',
-        name: 'Gold Crowns',
-        img: 'icons/commodities/currency/coin-embossed-crown-gold.webp',
-        abbreviation: '{#} gc',
+        type: "attribute",
+        name: "Gold Crowns",
+        img: "icons/commodities/currency/coin-embossed-crown-gold.webp",
+        abbreviation: "{#} gc",
         data: {
-          path: 'system.wealth.gc',
+          path: "system.wealth.gc",
         },
         primary: true,
         exchangeRate: 1,
       },
       {
-        type: 'attribute',
-        name: 'Silver Shillings',
-        img: 'icons/commodities/currency/coin-inset-compass-silver.webp',
-        abbreviation: '{#} ss',
+        type: "attribute",
+        name: "Silver Shillings",
+        img: "icons/commodities/currency/coin-inset-compass-silver.webp",
+        abbreviation: "{#} ss",
         data: {
-          path: 'system.wealth.ss',
+          path: "system.wealth.ss",
         },
         primary: false,
         exchangeRate: 0.1,
       },
       {
-        type: 'attribute',
-        name: 'Copper Pennies',
-        img: 'icons/commodities/currency/coin-engraved-waves-copper.webp',
-        abbreviation: '{#} cp',
+        type: "attribute",
+        name: "Copper Pennies",
+        img: "icons/commodities/currency/coin-engraved-waves-copper.webp",
+        abbreviation: "{#} cp",
         data: {
-          path: 'system.wealth.cp',
+          path: "system.wealth.cp",
         },
         primary: false,
         exchangeRate: 0.01,
       },
       {
-        type: 'attribute',
-        name: 'Bits',
-        img: 'icons/commodities/currency/coins-assorted-mix-platinum.webp',
-        abbreviation: '{#} bits',
+        type: "attribute",
+        name: "Bits",
+        img: "icons/commodities/currency/coins-assorted-mix-platinum.webp",
+        abbreviation: "{#} bits",
         data: {
-          path: 'system.wealth.bits',
+          path: "system.wealth.bits",
         },
         primary: false,
         exchangeRate: 0.001,
@@ -132,20 +132,7 @@ Hooks.once('item-piles-ready', async () => {
 
     SYSTEM_HOOKS: () => {},
 
-		"SHEET_OVERRIDES": () => {
-			const actorSheetOverride = 'CONFIG.Actor.sheetClasses.character["demonlord.DLCharacterSheet"].cls.prototype.render'
-
-			libWrapper.register("item-piles-demonlord", actorSheetOverride, function (wrapped, forced, options, ...args) {
-				const renderItemPileInterface = Hooks.call(game.itempiles.CONSTANTS.HOOKS.PRE_RENDER_SHEET, this.document, forced, options) === false;
-
-					if (renderItemPileInterface) {
-						wrapped(forced, options, ...args)
-					} else {
-						return wrapped(forced, options, ...args)
-					}
-				
-			}, "MIXED");
-		}    
+    SHEET_OVERRIDES: () => {}
   }
 
   await game.itempiles.API.addSystemIntegration(data)
